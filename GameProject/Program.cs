b54874947a37a9067d542b59f9d0019e72d6961e
Tuğçe Özelmaci <74603980+7tugce@users.noreply.github.com>
2024-08-01 10:14:08 +0300

    1.8.2024 10.12

---
+++ GameProject/Program.cs
@@ -0,0 +1,8 @@
+﻿
+using GameProject;
+
+GamerManager gamerManager = new GamerManager(new UserValidationManager();
+gamerManager.Add(new Gamer { Id = 1, BirthYear = 1999,
+    FirstName = "Tuğçe", LastName = "Özelmaci",
+    IdentityNumber = 1278
+});
---
+++ GameProject/UserValidationManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject
+{
+    internal class UserValidationManager : IUserValidationService
+    {
+        public bool Validate(Gamer gamer)
+        {
+           if(gamer.BirthYear == 1999 && gamer.FirstName=="Tuğçe" && gamer.LastName=="Özelmaci" && gamer.IdentityNumber==1278)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
